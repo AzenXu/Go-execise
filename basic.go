744d4bb82527e9b3f16c0d2b8720efd2dc502438
Azen Xu <xuchang08@meituan.com>
2018-07-11 16:22:54 +0800

    内置类型及常量定义

--- basic.go
+++ basic.go
@@ -2,21 +2,66 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 var (
-	s string
-	a, b int
+	s     string
+	a, bb int
 	// a, b := 666, 233 // 全局变量不能用:=定义
 )
 var ss = "总想搞个大新闻"
 
+const (
+	b = 1 << (10 * iota)
+	kb
+	mb
+	gb
+	tb
+	pb
+)
+
 func main() {
 	fmt.Println("Hello Azen٩(●˙▿˙●)۶…⋆ฺ")
+	//	常变量定义
 	variableZeroValueDefine()
 	varibaleInitial()
 	varibaleTypeDeduction()
 	varibaleShortcut()
+	triangle()
+	consts()
+	enums()
+}
+
+/*
+ * 常变量定义部分
+ */
+
+func enums() {
+	const (
+		azen = iota
+		daker
+		_
+		buran
+	)
+	fmt.Println(azen, daker, buran)
+}
+
+func consts() {
+	const (
+		filename = "abc.txt"
+		a, b     = 3, 4
+	)
+	var c int
+	c = int(math.Sqrt(a*a + b*b))
+	fmt.Println(filename, c)
+}
+
+func triangle() {
+	var a, b = 3, 4
+	var c int
+	c = int(math.Sqrt(float64(a*a + b*b)))
+	fmt.Println(c)
 }
 
 func variableZeroValueDefine() {
@@ -42,4 +87,3 @@ func varibaleShortcut() {
 	s := "老铁蛤蛤蛤"
 	fmt.Println(a, b, s)
 }
-
